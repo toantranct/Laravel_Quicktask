b2bc9b1f2a2c3fb85946534bca9231f174a3c651
toantranct <mxhios@gmail.com>
2023-01-12 08:47:47 +0700

    rebase main

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -27,6 +27,9 @@ class Task extends Model
     protected $casts = [
         'state' => 'boolean',
     ];
-}
 
+    public function user() {
+        return $this->belongsTo(User::class);
+    }
+}
 
